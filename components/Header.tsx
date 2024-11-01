1bb354d49d93b1cbc82412c6dc1b7bf714a129ee
frontcodelover <contact@avantjetaisriche.com>
2024-11-01 19:08:03 +0100

    FEAT: add notification design

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,18 +1,50 @@
 'use client';
 
+import React, { useState } from 'react';
 import { CommandDemo } from './Command';
 import { Button } from './ui/button';
 import { BellIcon } from 'lucide-react';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
 
 export const Header = () => {
+  const [notifications, setNotifications] = useState<any>([
+    {
+      text: 'You have 1 new message',
+      date: '2 hours ago',
+      read: true,
+    },
+    {
+      text: 'You have another new message',
+      date: '2 hours ago',
+      read: false,
+    },
+  ]);
+
   return (
     <div className='grid grid-cols-2 gap-4 p-4 border-b'>
-			<CommandDemo />
-			<div className='flex items-center justify-end'>
-				<Button variant="outline" size="icon">
-					<BellIcon />
-				</Button>
-			</div>
+      <CommandDemo />
+      <div className='flex items-center justify-end'>
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Button variant='outline' size='icon' className='relative'>
+							<div className={`absolute -top-2 -right-2 h-3 w-3 my-1 rounded-full ${notifications.find((x:any) => x.read === true) ? 'bg-green-500' : 'bg-transparent'}`} />
+							<BellIcon />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align='end'>
+            {notifications.map((notification: any, index: number) => (
+              <DropdownMenuItem key={index} className='p-1 bg-neutral-50 cursor-pointer transition flex items-start gap-2'>
+                <div className={`h-3 w-3 my-1 rounded-full ${notification.read ? 'bg-transparent' : 'bg-green-500'}`} />
+                <div>
+                  <p>{notification.text}</p>
+                  <p className='text-xs text-neutral-500'>{notification.date}</p>
+                </div>
+              </DropdownMenuItem>
+            ))}
+            <DropdownMenuSeparator />
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
     </div>
   );
 };
